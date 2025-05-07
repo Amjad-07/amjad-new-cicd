68dcd6693ad8de320268e4a077e08b77b537c131
Amjad-07 <146514436+Amjad-07@users.noreply.github.com>
2025-05-07 23:10:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+# app.py
 
-app.get('/', (req, res) => {
-  res.send('Hello World from Cloud Run!');
-});
+from flask import Flask
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
